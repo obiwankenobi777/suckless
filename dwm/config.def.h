bb3002ea1c029d64432009fb0f98a86f2e2076cf
obiwankenobi777 <ordemjedi7@gmail.com>
2024-12-21 18:10:23 -0300

    Added dark gray in ./dwm/config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "hasklug nerd font black:size=10" };
 static const char dmenufont[]       = { "hasklug nerd font black:size=10" };
 //static const char col_gray1[]       = "#222222";
@@ -13,9 +13,9 @@ static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#220022";
-//static const char col_cyan[]        = "#005577";
-//static const char col_cyan[]        = "#0088cc";
+static const char col_cyan[]        = "#1f242a";    //dark gray
+//static const char col_cyan[]        = "#0088cc";  //blue arch
+//static const char col_cyan[]        = "#005577";  //dwm
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },

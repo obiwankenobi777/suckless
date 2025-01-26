5154114fbb84dc3ae275cceb17bcd00c6939d941
obiwankenobi777 <ordemjedi7@gmail.com>
2025-01-25 21:42:56 -0300

    Update in st

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "hasklug nerd font black:pixelsize=16:antialias=true:autohint=true";
+static char *font = "hasklug nerd font black:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 3;
 
 /*
